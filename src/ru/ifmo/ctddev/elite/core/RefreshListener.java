05eabe48f77c5f4925522082b175797aad189da2
Daniyar Itegulov <wibkwibk@gmail.com>
2015-06-11 15:06:55 +0300

    Added RMI to RefreshListener

--- src/ru/ifmo/ctddev/elite/core/RefreshListener.java
+++ src/ru/ifmo/ctddev/elite/core/RefreshListener.java
@@ -1,9 +1,11 @@
 package ru.ifmo.ctddev.elite.core;
 
+import java.rmi.Remote;
+
 /**
  * @author Daniyar Itegulov
  */
 @FunctionalInterface
-public interface RefreshListener {
+public interface RefreshListener extends Remote {
     void onRefresh();
 }
